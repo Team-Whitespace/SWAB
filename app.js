21019b96b3d3c48068e2e06506d63ef676c7619d
Kiran J Hampal <kiran@elementaryos.org>
2015-02-05 15:22:34 +0000

    Renamed routes to controllers, and removed unused users stuff

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var controllers = {
+    tweets: require('./controllers/tweets')
+}
 
 var app = express();
 
@@ -22,8 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', controllers.tweets);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
