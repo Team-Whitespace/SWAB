58d6d20dac1e884eb1e14924492631fd1ef559e3
Kiran J Hampal <kiran@elementaryos.org>
2015-02-06 23:50:07 +0000

    Simplifications

--- models/fake_solr.js
+++ models/fake_solr.js
@@ -1,22 +1,12 @@
 "use strict";
 
-var sampleData = {
-	china: require('../data/china.json'),
-	chinese: require('../data/chinese.json'),
-	premium: require('../data/premium.json'),
-	post: require('../data/post.json'),
-	oil: require('../data/oil.json'),
-	diamond: require('../data/diamond.json'),
-	rio: require('../data/rio.json'),
-	streamline: require('../data/streamline.json')
-}
+var sampleData = require('../data/sampletweets.json')
 
 function getTweets(keyword) {
-	var arrayObj = sampleData[keyword];
-  var tweetArray = arrayObj.statuses;
-  return tweetArray;
+  var tweets = sampleData[keyword];
+  return tweets.statuses;
 }
 
 module.exports = {
-	getTweets: getTweets
+  getTweets: getTweets
 }
