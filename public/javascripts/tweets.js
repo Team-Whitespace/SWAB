25410764d155321b4fb09c1fe0b7c38e5e099cc5
William Andrew Blakey <wab9000@hotmail.com>
2015-02-06 21:53:44 +0000

    Added adding alert functionality to the frontend, and tweets display more information

--- public/javascripts/tweets.js
+++ public/javascripts/tweets.js
@@ -1,14 +1,34 @@
+"use strict"
 
-getTweets('china');
+var socket = io.connect(location.protocol + '//' + location.host);
 
-function getTweets(alert) {
-    var socket = io.connect(location.protocol + '//' + location.host);
-    socket.emit('join', alert);
-    socket.on(alert, function displayTweet(data) {
-        document.getElementById('contentList').innerHTML =
-            '<li>'+
-            data.tweet.text +
-            '</li>' +
-            document.getElementById('contentList').innerHTML;
-    });
+getTweets('oil');
+
+document.getElementById("subButton").addEventListener("click", sentAlert);
+
+function sentAlert() {
+  var text = document.getElementById('alertSub').value;
+  socket.emit('addAlert', text);
+  document.getElementById('alertList').innerHTML =
+    '<li>' +
+      '<a href="#" >' + text + '</a>' +
+    '</li>' +
+    document.getElementById('alertList').innerHTML;
+}
+
+
+function getTweets(tweet) {
+  socket.emit('join', tweet);
+  socket.on(tweet, function displayTweet(data) {
+    document.getElementById('content').innerHTML =
+      '<div id = "pictureAndName">'+
+        '<img src ="' + data.tweet.user.profile_image_url + '">' +
+        '<p>' + data.tweet.user.name + '</p>' +
+      '</div>' +
+      '<div id = "tweetContent">'+
+        '<p>' + data.tweet.text + '</p>' +
+        '<p>' + data.tweet.created_at + '</p>' +
+      '</div>' +
+      document.getElementById('content').innerHTML;
+  });
 }
